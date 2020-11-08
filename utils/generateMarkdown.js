4b44f9790c7593eb8232fd4507f20831836b33ae
Nancy Lewis <nancylewis11295@gmail.com>
2020-11-08 15:53:16 -0500

    updating files

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,16 +1,53 @@
-// function passing in liscense as an arg
-// validate lisense exists
-// return ![GitHub license](https://img.shields.io/badge/license- ROUTE HERE -blue.svg)
-
-
 // function to generate markdown for README
+function renderLicenseBadge(license){
+  if(license !== "none"){
+    return '![GitHub license](https://img.shields.io/badge/license-${license}-blue.svg)'
+  }
+  else{
+    console.log("you did not pick a license")
+  }
+}
+function renderLicenseLink(license){
+    if(license !== "none"){
+      return '\n [License](#license)\n'
+    }
+}
+function renderLicenseSection(license){
+    if(license !== "none"){
+      return(
+        'license ## This repo is licensed under ${license} license'
+      )
+    }
+    else{
+      return ''
+    }
+}
+
+
 function generateMarkdown(data) {
   return `# ${data.title}
-  ${data.gitub}
+  ${renderLicenseBadge(data.license)}
 
-  ## Project Name
+  ## Description
+  ${data.description}
 
-  ${data.projectName}
+  ## Table of contents
+  ${data.questions}
+
+  ## Installation
+  ${data.dependencies}
+
+  ## Usage
+  ${data.repo}
+
+  ## Contributing
+  ${data.contribution}
+
+  ##Tests
+  ${data.tests}
+
+  ##Questions
+  'If you have any questions about the repo, please contact me at ${data.email}'
 `;
 }
 
